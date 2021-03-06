b45a23c3582a89a8a3dce5cffa03581461c61f90
Chris Battarbee <chris@chrisbattarbee.com>
2021-03-06 20:14:31 +0000

    Add new docker images

---
+++ scripts/self-magritte/build_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ex
+cd "$(dirname "${BASH_SOURCE[0]}" )"/../..
+
+VERSION=$(git describe --tags --always --first-parent)
+DEST=build/docker-self-magritte
+
+rm -rf $DEST
+mkdir -p $DEST/self-magritte/bin
+
+cp ./scripts/self-magritte/Dockerfile $DEST/
+cp ./self-magritte/build/self-magritte $DEST/self-magritte/bin/self-magritte
+
+cd $DEST
+docker build -t "chrisbattarbee/self-magritte:$VERSION" .
+docker tag "chrisbattarbee/self-magritte:$VERSION" "chrisbattarbee/self-magritte:latest"
+docker push chrisbattarbee/self-server:latest
