08118bd796dc0240870517690333a70ab70053c7
Chris Battarbee <chris@chrisbattarbee.com>
2021-03-06 20:48:22 +0000

    Install python api

--- self-magritte/setup.py
+++ self-magritte/setup.py
@@ -1,7 +1,7 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
     name='self-magritte',
     version='0.0.1',
     package_dir={'self_magritte': 'src'},
-    packages=['self_magritte'],
+    packages=find_packages()
 )
