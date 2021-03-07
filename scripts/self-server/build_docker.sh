e9959717ee0a3de37a6d0be0f3a6f9e8e0960cc1
Chris Battarbee <chris@chrisbattarbee.com>
2021-03-07 17:26:56 +0000

    Fix typo =

--- scripts/self-server/build_docker.sh
+++ scripts/self-server/build_docker.sh
@@ -10,7 +10,7 @@ mkdir -p $DEST/self-server/var/conf
 
 cp ./scripts/self-server/Dockerfile $DEST/
 tar -xf ./self-server/build/distributions/self-server-*.tar -C $DEST/self-server --strip-components=1
-cp ./self-server/var/conf/recipes.yml $DEST/self-server/var/conf
+cp ./self-server/var/conf/selfserver.yml $DEST/self-server/var/conf
 
 cd $DEST
 docker build -t "chrisbattarbee/self-server:$VERSION" .
