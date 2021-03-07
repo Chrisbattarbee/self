695d91a0d6acf9e948856db9f8bfe9e64d445407
Chris Battarbee <chris@chrisbattarbee.com>
2021-03-07 00:59:08 +0000

    Initial mfp implementation

---
+++ self-magritte/src/calories/__init__.py
@@ -0,0 +1,69 @@
+import myfitnesspal
+import conf
+from self_api.self_calories import CalorieService, FoodEntry, Meal, MacroGoals
+from conjure_python_client import RequestsClient, ServiceConfiguration
+import datetime
+
+
+def current_iso_date():
+    return datetime.datetime.now().date()
+
+
+def get_self_api_calories_client():
+    config = ServiceConfiguration()
+    config.uris = [conf.calories_conf['self_api_server']]
+    client = RequestsClient.create(CalorieService, user_agent="self-magritte", service_config=config)
+    return client
+
+
+def get_current_day_logs():
+    client = myfitnesspal.Client(username=conf.calories_conf['username'], password=conf.calories_conf['password'])
+    log = client.get_date(2020, 3, 1)
+    log.goals['calories']
+    return log
+
+
+def convert_mfp_day_logs_to_self_api_format(day_logs):
+    macro_goals = MacroGoals(
+        calories=day_logs.goals['calories'],
+        carbohydrates=day_logs.goals['carbohydrates'],
+        fat=day_logs.goals['fat'],
+        protein=day_logs.goals['protein'],
+        sodium=day_logs.goals['sodium'],
+        sugar=day_logs.goals['sugar']
+    )
+
+    meals = []
+    for meal in day_logs.meals:
+        food_entries = []
+        for food_entry in meal.entries:
+            food_entries.append(
+                FoodEntry(
+                    name=food_entry.name,
+                    quantity=food_entry.quantity,
+                    unit=food_entry.unit,
+                    calories=food_entry.totals['calories'],
+                    carbohydrates=food_entry.totals['carbohydrates'],
+                    fat=food_entry.totals['fat'],
+                    protein=food_entry.totals['protein'],
+                    sodium=food_entry.totals['sodium'],
+                    sugar=food_entry.totals['sugar'],
+                )
+            )
+        meals.append(
+            Meal(
+                name=meal.name,
+                entries=food_entries,
+            )
+        )
+
+
+    return meals, macro_goals
+
+
+logs = get_current_day_logs()
+meals, macro_goals = convert_mfp_day_logs_to_self_api_format(logs)
+self_api_client = get_self_api_calories_client()
+
+self_api_client.update_daily_calories(current_iso_date().isoformat(), meals)
+self_api_client.update_daily_macro_goals(current_iso_date().isoformat(), macro_goals)
--- self-magritte/src/lib.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-def test():
-    r = requests.get('https://google.com', )
-    print(r.text)
