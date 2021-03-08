74a7a918bef999d93154011de73c7a5ab2b46cfb
Chris Battarbee <chris@chrisbattarbee.com>
2021-03-08 00:41:08 +0000

    Clean up pex nonsense

--- self-magritte/setup.py
+++ self-magritte/setup.py
@@ -3,5 +3,5 @@ setup(
     name='self-magritte',
     version='0.0.1',
     package_dir={'self_magritte': 'src'},
-    packages=['self_magritte']
+    packages=['self_magritte', 'self_magritte.calories']
 )
