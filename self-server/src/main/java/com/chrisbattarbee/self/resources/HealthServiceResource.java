f088960f6bd833e02393e56ce3898110145b7d73
Chris Battarbee <Chrisbattarbee@users.noreply.github.com>
2022-04-03 00:24:08 +0100

    Add apple health service integration (#35)
    * Add Apple Health service api
    
    * Fix conjure dep
    
    * Add AppleHealthServiceResource
    
    * Implement the put endpoint
    
    * Update service dynamo name
    
    * Implement getHealthDataPoint
    
    * ParseDateAndTime
    
    * Spotless
    
    * Final

---
+++ self-server/src/main/java/com/chrisbattarbee/self/resources/HealthServiceResource.java
@@ -0,0 +1,73 @@
+package com.chrisbattarbee.self.resources;
+
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.chrisbattarbee.self.health.HealthService;
+import com.chrisbattarbee.self.workouts.HealthDataPoint;
+import com.chrisbattarbee.self.workouts.PostHealthData;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+import com.palantir.logsafe.SafeArg;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class HealthServiceResource extends SelfResource implements HealthService {
+    private static final String HEALTH_DYNAMO_TABLE = "self_api_health";
+    private static final String TABLE_KEY = "key";
+    private static final long READ_THROUGHPUT = 1L;
+    private static final long WRITE_THROUGHPUT = 1L;
+    private static final ProvisionedThroughput PROVISIONED_THROUGHPUT =
+            new ProvisionedThroughput(READ_THROUGHPUT, WRITE_THROUGHPUT);
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void putHealthData(PostHealthData data) {
+        logger.info("Received request to put health data");
+        data.getData()
+                .getMetrics()
+                .forEach(healthMetric -> healthMetric.getData().forEach(healthMetricInner -> super.getDynamoManager()
+                        .putObjectIntoDynamo(
+                                HEALTH_DYNAMO_TABLE,
+                                HealthDataPoint.builder()
+                                        .name(healthMetric.getName())
+                                        .unit(healthMetric.getUnits())
+                                        .date(parseDateFromDateTime(healthMetricInner.getDate()))
+                                        .amount(healthMetricInner.getQty())
+                                        .key(buildKey(
+                                                healthMetric.getName(),
+                                                parseDateFromDateTime(healthMetricInner.getDate())))
+                                        .build())));
+    }
+
+    @Override
+    public List<HealthDataPoint> getHealthDataInRange(String metricName, String startDate, String endDate) {
+        logger.info(
+                "Received request to get {} between {} and {}",
+                SafeArg.of("metricName", metricName),
+                SafeArg.of("startDate", startDate),
+                SafeArg.of("endDate", endDate));
+
+        List<String> keys = Utils.getDatesInRange(startDate, endDate).stream()
+                .map(date -> buildKey(metricName, date))
+                .collect(Collectors.toList());
+
+        return super.getDynamoManager()
+                .getObjectsFromDynamoBatched(HEALTH_DYNAMO_TABLE, TABLE_KEY, keys, HealthDataPoint.class);
+    }
+
+    @Override
+    void createDynamoTablesIfTheyDontExist() {
+        super.getDynamoManager()
+                .createDynamoTableIfItDoesntExist(HEALTH_DYNAMO_TABLE, TABLE_KEY, PROVISIONED_THROUGHPUT);
+    }
+
+    private static String parseDateFromDateTime(String dateTime) {
+        return Iterables.get(Splitter.on(' ').split(dateTime), 0);
+    }
+
+    private static String buildKey(String metricName, String date) {
+        return String.format("%s-%s", metricName, date);
+    }
+}
