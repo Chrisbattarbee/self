593110909ed81990cbab211e6fec255c25cedc67
Chris Battarbee <chris@chrisbattarbee.com>
2022-04-03 19:40:41 +0100

    Remove unused imports

--- self-frontend/src/Health/HealthChart.tsx
+++ self-frontend/src/Health/HealthChart.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-    Crosshair,
     HorizontalGridLines,
     LineSeries,
     LineSeriesPoint,
--- self-frontend/src/Health/HealthPanel.tsx
+++ self-frontend/src/Health/HealthPanel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {DefaultHttpApiBridge} from "conjure-client";
-import {Container, Divider} from "semantic-ui-react";
+import {Container} from "semantic-ui-react";
 import {HealthService} from "conjure-self-api/self-health/healthService";
 import {IHealthDataPoint} from "conjure-self-api/self-workouts/healthDataPoint";
 import {HealthChart} from "./HealthChart";
